058860f2072d6fd71f3405e4ca569cb126124eaa
Zachary Grauerholz <20744813+ZGrauer@users.noreply.github.com>
2023-02-10 11:19:18 -0600

    Add files via upload

--- src/app/members/members.component.ts
+++ src/app/members/members.component.ts
@@ -1,6 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 
-import { HttpRequest, HttpResponse, HttpHandler, HttpEvent, HttpInterceptor, HttpErrorResponse, HttpHeaders } from '@angular/common/http';
+import {
+  HttpRequest,
+  HttpResponse,
+  HttpHandler,
+  HttpEvent,
+  HttpInterceptor,
+  HttpErrorResponse,
+  HttpHeaders,
+} from '@angular/common/http';
 import { Router } from '@angular/router';
 
 import { AuthService } from '../auth.service';
@@ -8,20 +16,44 @@ import { AuthService } from '../auth.service';
 @Component({
   selector: 'app-members',
   templateUrl: './members.component.html',
-  styleUrls: ['./members.component.css']
+  styleUrls: ['./members.component.css'],
 })
 export class MembersComponent {
-  pdfSrc = "../../assets/Kansas_City_St_Andrew_Pipe_Band_CONSTITUTION.pdf";
+  bandConstitutionPdfSrc = '../../assets/Kansas_City_St_Andrew_Pipe_Band_CONSTITUTION.pdf';
+  bandConstitutionPdfFilename = 'Kansas_City_St_Andrew_Pipe_Band_CONSTITUTION.pdf';
 
-  constructor(private _authService: AuthService, private router: Router) { }
+  drumMajorPdfSrc = '../../assets/drum_major_manual.pdf';
+  drumMajorPdfFilename = 'drum_major_manual.pdf';
+
+  constructor(private _authService: AuthService, private router: Router) {}
 
   ngOnInit() {
     this.isLoggedIn();
   }
 
-  isLoggedIn(){
-    if(!this._authService.loggedIn()) {
+  isLoggedIn() {
+    if (!this._authService.loggedIn()) {
       this.router.navigate(['/login']);
     }
+  }
+
+  downloadBandConstitutionPDF() {
+    const link = document.createElement('a');
+    link.setAttribute('target', '_blank');
+    link.setAttribute('href', this.bandConstitutionPdfSrc);
+    link.setAttribute('download', this.bandConstitutionPdfFilename);
+    document.body.appendChild(link);
+    link.click();
+    link.remove();
+  }
+
+  downloadDrumMajorPDF() {
+    const link = document.createElement('a');
+    link.setAttribute('target', '_blank');
+    link.setAttribute('href', this.drumMajorPdfSrc);
+    link.setAttribute('download', this.drumMajorPdfFilename);
+    document.body.appendChild(link);
+    link.click();
+    link.remove();
   }
 }
